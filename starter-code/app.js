4f30e84733558504eb6efea1ee4d9c255d5d26b7
Manuel del Pino Lucena <manueldelpinolucena@gmail.com>
2017-09-14 18:58:27 +0200

    starting

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,12 +1,15 @@
-const express        = require("express");
-const session        = require("express-session");
+const express = require("express");
+const session = require("express-session");
 const expressLayouts = require("express-ejs-layouts");
-const path           = require("path");
-const logger         = require("morgan");
-const cookieParser   = require("cookie-parser");
-const bodyParser     = require("body-parser");
-const mongoose       = require("mongoose");
-const app            = express();
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+
+const index = require('./routes/index');
+
 
 // Controllers
 
@@ -20,12 +23,14 @@ app.use(logger("dev"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(expressLayouts);
-app.set("layout", "layouts/main-layout");
+app.set("layout", "layouts");
 app.use(express.static(path.join(__dirname, "public")));
 
 // Access POST params with body parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 // Authentication
 app.use(session({
@@ -34,7 +39,7 @@ app.use(session({
 app.use(cookieParser());
 
 // Routes
-// app.use("/", index);
+app.use(index);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
