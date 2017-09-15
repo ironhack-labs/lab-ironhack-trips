6a6ab2d121f66b6b4371a9137ec4c7c48687139e
Francisco Paradela <fparafer@gmail.com>
2017-09-15 09:18:04 +0200

    Iteration 1 done

--- starter-code/app.js
+++ starter-code/app.js
@@ -7,6 +7,11 @@ const cookieParser   = require("cookie-parser");
 const bodyParser     = require("body-parser");
 const mongoose       = require("mongoose");
 const app            = express();
+const FbStrategy     = require('passport-facebook').Strategy;
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./models/User")
 
 // Controllers
 
@@ -15,12 +20,39 @@ mongoose.connect("mongodb://localhost/ironhack-trips");
 
 // Middlewares configuration
 app.use(logger("dev"));
+passport.use(new FbStrategy({
+  clientID: "1507402949353665",
+  clientSecret: "b7bdd4521ad7f7b0135cc051c737e0fb",
+  callbackURL: "/auth/facebook/callback"
+}, (accessToken, refreshToken, profile, done) => {
+  User.findOne({ provider_id: profile.id }, (err, user) => {
+    if (err) {
+      return done(err);
+    }
+    if (user) {
+      return done(null, user);
+    }
+
+    const newUser = new User({
+      provider_id: profile.id,
+      provider_name: profile.displayName
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        return done(err);
+      }
+      done(null, newUser);
+    });
+  });
+
+}));
 
 // View engine configuration
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(expressLayouts);
-app.set("layout", "layouts/main-layout");
+app.set("layout", "layout");
 app.use(express.static(path.join(__dirname, "public")));
 
 // Access POST params with body parser
@@ -33,8 +65,47 @@ app.use(session({
 }));
 app.use(cookieParser());
 
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Routes
 // app.use("/", index);
+const authRoutes = require("./routes/auth-routes");
+app.use('/', authRoutes);
+authRoutes.get("/auth/facebook", passport.authenticate("facebook"));
+authRoutes.get("/auth/facebook/callback", passport.authenticate("facebook", {
+  successRedirect: "/private-page",
+  failureRedirect: "/"
+}));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
