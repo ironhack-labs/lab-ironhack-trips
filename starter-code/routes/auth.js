ec16d12b39be58a0512602647f5b11d824eea417
VeroGlez <veronicagonzalezperez@MacBook-Pro-de-Veronica.local>
2017-09-15 09:16:43 +0200

    Starting iteration 2

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -23,55 +23,5 @@ router.get('/logout',(req,res) =>{
   res.redirect("/");
 });
 
-// router.post("/signup", (req, res, next) => {
-//   const username = req.body.username;
-//   const password = req.body.password;
-//
-//   if (username === "" || password === "") {
-//     res.render("auth/signup", { message: "Indicate username and password" });
-//     return;
-//   }
-//
-//   User.findOne({ username }, "username", (err, user) => {
-//     if (user !== null) {
-//       res.render("auth/signup", { message: "The username already exists" });
-//       return;
-//     }
-//
-//     const salt = bcrypt.genSaltSync(bcryptSalt);
-//     const hashPass = bcrypt.hashSync(password, salt);
-//
-//     debug("User created");
-//
-//     const newUser = new User({
-//       username,
-//       password: hashPass
-//     })
-//     .save()
-//     .then(user => res.redirect('/'))
-//     .catch(e => res.render("auth/signup", { message: "Something went wrong" }));
-//
-//   });
-// });
-//
-//
-// router.get('/login',(req,res) =>{
-//   res.render('auth/login',{ message: req.flash("error") });
-// });
-//
-// router.post("/login", passport.authenticate("local", {
-//   successRedirect: "/",
-//   failureRedirect: "/login",
-//   failureFlash: true,
-//   passReqToCallback: true
-// }));
-//
-// router.post('/logout',(req,res) =>{
-//   req.logout();
-//   res.redirect("/");
-// });
-//
-//
-
 
 module.exports = router;
