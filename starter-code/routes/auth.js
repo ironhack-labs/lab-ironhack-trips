2a989a848103eda9c07222afeef80c20890db4c1
VeroGlez <veronicagonzalezperez@MacBook-Pro-de-Veronica.local>
2017-09-15 09:14:28 +0200

    Starting iteration 2

---
+++ starter-code/routes/auth.js
@@ -0,0 +1,77 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+const path = require('path');
+const passport = require('passport');
+const debug = require('debug')("app:auth:local");
+
+const router = require('express').Router();
+
+router.get("/", (req, res, next) => {
+  console.log(req.user)
+  res.render("index");
+});
+
+router.get("/auth/facebook", passport.authenticate("facebook"));
+router.get("/auth/facebook/callback", passport.authenticate("facebook", {
+  successRedirect: "/my-trips",
+  failureRedirect: "/"
+}));
+
+router.get('/logout',(req,res) =>{
+  req.logout();
+  res.redirect("/");
+});
+
+// router.post("/signup", (req, res, next) => {
+//   const username = req.body.username;
+//   const password = req.body.password;
+//
+//   if (username === "" || password === "") {
+//     res.render("auth/signup", { message: "Indicate username and password" });
+//     return;
+//   }
+//
+//   User.findOne({ username }, "username", (err, user) => {
+//     if (user !== null) {
+//       res.render("auth/signup", { message: "The username already exists" });
+//       return;
+//     }
+//
+//     const salt = bcrypt.genSaltSync(bcryptSalt);
+//     const hashPass = bcrypt.hashSync(password, salt);
+//
+//     debug("User created");
+//
+//     const newUser = new User({
+//       username,
+//       password: hashPass
+//     })
+//     .save()
+//     .then(user => res.redirect('/'))
+//     .catch(e => res.render("auth/signup", { message: "Something went wrong" }));
+//
+//   });
+// });
+//
+//
+// router.get('/login',(req,res) =>{
+//   res.render('auth/login',{ message: req.flash("error") });
+// });
+//
+// router.post("/login", passport.authenticate("local", {
+//   successRedirect: "/",
+//   failureRedirect: "/login",
+//   failureFlash: true,
+//   passReqToCallback: true
+// }));
+//
+// router.post('/logout',(req,res) =>{
+//   req.logout();
+//   res.redirect("/");
+// });
+//
+//
+
+
+module.exports = router;
