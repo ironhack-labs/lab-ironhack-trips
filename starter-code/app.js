01102b5f57cd04fb968a85c500d5dd1bc4c7babe
Thomas <tom.beguin@gmail.com>
2017-10-09 09:24:55 +0200

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,14 +1,45 @@
-const express        = require("express");
-const session        = require("express-session");
+require('dotenv').config()
+const express = require("express");
+const session = require("express-session");
 const expressLayouts = require("express-ejs-layouts");
-const path           = require("path");
-const logger         = require("morgan");
-const cookieParser   = require("cookie-parser");
-const bodyParser     = require("body-parser");
-const mongoose       = require("mongoose");
-const app            = express();
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo")(session);
+const passport = require('passport')
+const User = require('./models/users');
+const Trip = require('./models/trip');
+const FacebookStrategy = require('passport-facebook').Strategy;
+const bcrypt = require('bcrypt');
+
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/callback",
+    profileFields: ['id', 'displayName']
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function(err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
 
 // Controllers
+const app = express();
+
+const auth = require('./routes/auth.js')
 
 // Mongoose configuration
 mongoose.connect("mongodb://localhost/ironhack-trips");
@@ -20,21 +51,25 @@ app.use(logger("dev"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(expressLayouts);
-app.set("layout", "layouts/main-layout");
+app.set("layout", "../layouts/main-layout");
 app.use(express.static(path.join(__dirname, "public")));
 
 // Access POST params with body parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
 
 // Authentication
 app.use(session({
-  secret: "ironhack trips"
+  secret: "ironhack trips",
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
 }));
 app.use(cookieParser());
 
 // Routes
-// app.use("/", index);
+
+app.use("/", auth);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
