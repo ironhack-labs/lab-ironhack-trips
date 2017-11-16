508cd744d35cbd1ab6766be023732b69d009f2bc
gemaderus <hi@gemaderus.com>
2017-11-16 23:39:29 +0100

    First iteration and half second iteration.

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,57 +1,106 @@
-const express        = require("express");
-const session        = require("express-session");
-const expressLayouts = require("express-ejs-layouts");
-const path           = require("path");
-const logger         = require("morgan");
-const cookieParser   = require("cookie-parser");
-const bodyParser     = require("body-parser");
-const mongoose       = require("mongoose");
-const app            = express();
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+const expressLayouts     = require('express-ejs-layouts');
+const session            = require('express-session');
+const mongoose           = require('mongoose');
+const MongoStore         = require('connect-mongo')(session);
+mongoose.connect('mongodb://localhost:27017/tumblr-lab-development');
+const FbStrategy = require('passport-facebook').Strategy;
+var app = express();
 
-// Controllers
 
-// Mongoose configuration
-mongoose.connect("mongodb://localhost/ironhack-trips");
-
-// Middlewares configuration
-app.use(logger("dev"));
-
-// View engine configuration
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main-layout');
 app.use(expressLayouts);
-app.set("layout", "layouts/main-layout");
-app.use(express.static(path.join(__dirname, "public")));
 
-// Access POST params with body parser
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// Authentication
-app.use(session({
-  secret: "ironhack trips"
-}));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
-// app.use("/", index);
+//Passport
+const passport   = require('passport');
+
+//Facebook
+passport.authenticate("facebook");
+
+
+app.get("/logout", (req, res) => {
+  req.logout()
+  res.redirect("/auth/login")
+})
+
+app.get("/facebook", passport.authenticate("facebook"));
+
+app.get("/facebook/callback", passport.authenticate("facebook", {
+  successRedirect: "/trips",
+  failureRedirect: "/",
+}));
+
+passport.use(new FbStrategy({
+  clientID: "151536418796740",
+  clientSecret: "d1e641e18bc3740aebf496d0bb8cd6b0",
+  callbackURL: "/auth/facebook/callback"
+}, (accessToken, refreshToken, profile, done) => {
+  User.findOne({ facebookID: profile.id }, (err, user) => {
+    if (err) {
+      return done(err);
+    }
+    if (user) {
+      return done(null, user);
+    }
+
+    const newUser = new User({
+      facebookID: profile.id
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        return done(err);
+      }
+      done(null, newUser);
+    });
+  });
+
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+var index = require('./routes/index');
+// var users = require('./routes/users');
+const authRoutes = require('./routes/auth');
+const trips = require('./routes/trips');
+
+app.use("/", index)
+app.use("/", authRoutes)
+app.use("/trips", trips)
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error("Not Found");
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
---
+++ starter-code/routes/auth.js
@@ -0,0 +1,22 @@
+var express = require('express');
+const passport   = require('passport');
+var authRoutes = express.Router();
+const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const FbStrategy = require('passport-facebook').Strategy;
+passport.authenticate("facebook");
+passport.authenticate("facebook");
+
+// authRoutes.get("/logout", (req, res) => {
+//   req.logout()
+//   res.redirect("/auth/login")
+// })
+
+// authRoutes.get("/facebook", passport.authenticate("facebook"));
+//
+// authRoutes.get("/facebook/callback", passport.authenticate("facebook", {
+//   successRedirect: "/trips",
+//   failureRedirect: "/",
+// }));
+//
+//
+module.exports = authRoutes;
