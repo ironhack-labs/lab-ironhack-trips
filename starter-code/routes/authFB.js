2ad17fcc4ff148930b176573b9e665edf44ef59c
Rocio Garcia <gooblyn@gmail.com>
2017-11-17 01:20:21 +0100

    Some modifications

--- starter-code/routes/authFB.js
+++ starter-code/routes/authFB.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
+const ensureLogin = require('connect-ensure-login');
 
 router.get("/facebook", passport.authenticate("facebook"));
 
 router.get("/facebook/callback", passport.authenticate("facebook", {
   successRedirect: "/trips",
-  failureRedirect: "/"
+  failureRedirect: "/index"
 }));
+
+router.post('/logout', ensureLogin.ensureLoggedIn('/login'), (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
 
 
 module.exports = router;
--- starter-code/routes/trips.js
+++ starter-code/routes/trips.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
+const ensureLogin = require('connect-ensure-login');
 
-router.get("/", (req, res, next) => {
+router.get("/", ensureLogin.ensureLoggedIn(), (req, res, next) => {
   res.render("layouts/my-trips");
 });
 
