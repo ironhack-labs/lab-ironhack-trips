3c0b1f1f55004f1802715f70e35735e352a2ef65
pablohk <pablusmmid@gmail.com>
2017-11-19 11:12:39 +0100

    delete facebook id

--- starter-code/passport/facebookStrategy.js
+++ starter-code/passport/facebookStrategy.js
@@ -3,8 +3,8 @@ const User = require('../models/User');
 const FaceBookStrategy = require("passport-facebook").Strategy;
 
 passport.use(new FaceBookStrategy({
-  clientID: "930574457098224",
-  clientSecret: "dee8d32f70ccbfe2a7a7181b5222c287",
+  clientID: "-",
+  clientSecret: "-",
   callbackURL: "authFacebook/callback"
 }, (accessToken, refreshToken, profile, done) => {
   console.log(profile);
