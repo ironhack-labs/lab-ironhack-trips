fcc1b1149b71a4e1c349ede24e98fbb3253bc81d
Beltran Rengifo <merloc@gmail.com>
2018-02-08 21:27:04 +0100

    login done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,15 +1,17 @@
-const express        = require("express");
-const session        = require("express-session");
+const express = require("express");
+const session = require("express-session");
 const expressLayouts = require("express-ejs-layouts");
-const path           = require("path");
-const logger         = require("morgan");
-const cookieParser   = require("cookie-parser");
-const bodyParser     = require("body-parser");
-const mongoose       = require("mongoose");
-const app            = express();
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const passportConfig = require('./passport')
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
 
 // Controllers
-
+const index = require('./routes/index');
+const authRoutes = require('./routes/auth');
 // Mongoose configuration
 mongoose.connect("mongodb://localhost/ironhack-trips");
 
@@ -20,12 +22,14 @@ app.use(logger("dev"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(expressLayouts);
-app.set("layout", "layouts/main-layout");
+app.set("layout", "layouts/main");
 app.use(express.static(path.join(__dirname, "public")));
 
 // Access POST params with body parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 // Authentication
 app.use(session({
@@ -33,8 +37,19 @@ app.use(session({
 }));
 app.use(cookieParser());
 
+//very handy locals
+app.use((req, res, next) => {
+  res.locals = {
+    title: 'Travel Log',
+    user: req.user || null
+  }
+  next();
+});
+passportConfig(app);
 // Routes
-// app.use("/", index);
+app.use("/", index);
+app.use('/facebook', authRoutes);
+app.use('/logout', authRoutes);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
---
+++ starter-code/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const isLoggedIn = require('../middlewares/isLoggedIn');
+const onlyMe = require('../middlewares/onlyMe');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {user:req.user});
+});
+
+module.exports = router;
